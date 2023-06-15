ebb7394765c11aab05c089108099c8f4e3369125
rahul.rauniyar <rahul.rauniyar@otssolutions.com>
2023-06-15 14:08:19 +0530

    replace /

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,3 +1,3 @@
 // const baseUrl = 'http://localhost:8000';
-const baseUrl = 'https://chat-app-backend-tzmq.onrender.com/';
+const baseUrl = 'https://chat-app-backend-tzmq.onrender.com';
 export default baseUrl;
